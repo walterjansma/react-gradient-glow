67133a0a1407804a3148b71556f3133e2c085934
developedbyed <ed.magician@gmail.com>
2025-02-08 18:26:21 +0000

    glow initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Nav from '@/components/nav'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${geistSans.className} antialiased`}
       >
-        {children}
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+        >
+          <Nav />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
